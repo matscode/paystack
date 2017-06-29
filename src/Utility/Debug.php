32f2595a620b83e6a5d1cd39a089720a1bee727a
Michael Akanji <matscode@gmail.com>
2017-06-29 17:31:00 +0100

    Created a method to debug string values printStr()

--- src/Utility/Debug.php
+++ src/Utility/Debug.php
@@ -21,6 +21,13 @@
 			$openTag = '<pre style="overflow: auto; max-height: 70%; max-width: 95%; position: fixed; z-index: 9999; left: 15px; top: 15px; padding: 15px; background-color: #fcfcfc; border: solid 1px #aaa; line-height: 1.1rem;">',
 			$closeTag = '</pre>';
 
+		public static function printStr( $value )
+		{
+			echo self::$openTag .
+			     $value .
+			     self::$closeTag;
+		}
+
 		public static function print_r( $value )
 		{
 			echo self::$openTag .
